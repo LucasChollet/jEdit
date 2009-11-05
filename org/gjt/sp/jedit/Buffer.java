LucasChollet/jEdit

commit 6619784b360f29aff1369fe0c83f3778389678f1

Fixed bug #2887040: Performance regression when splitting views (r16407).
git-svn-id: svn+ssh://svn.code.sf.net/p/jedit/svn/jEdit/trunk@16420 6b1eeb88-9816-0410-afa2-b43733a0f04e

org/gjt/sp/jedit/Buffer.java
@@ -2015,6 +2015,7 @@ public class Buffer extends JEditBuffer
 							bufferSets.add(bufferSet);
 					}
 				});
+				jEdit.getBufferSetManager().removeBuffer(this);
 				for (BufferSet bufferSet: bufferSets)
 					jEdit.getBufferSetManager().addBuffer(bufferSet, this);
 			}

org/gjt/sp/jedit/bufferset/BufferSet.java
@@ -97,21 +97,11 @@ public class BufferSet
 
 			if (sorter != null)
 			{
-				int prevPosition = -1;
 				if (buffers.contains(buffer))
-					prevPosition =  buffers.indexOf(buffer);
-				else
-					buffers.add(buffer);
+					return;
+				buffers.add(buffer);
 				Collections.sort(buffers, sorter);
 				position = buffers.indexOf(buffer);
-				if ((prevPosition != -1) && (position != prevPosition))
-				{
-					BufferSetListener[] listeners = this.listeners.getListeners(BufferSetListener.class);
-					Log.log(Log.DEBUG, this, hashCode() + ": Buffer moved " + buffer + " from " + prevPosition + " to " + position);
-					for (BufferSetListener listener : listeners)
-						listener.bufferMoved(buffer, prevPosition, position);
-					return;
-				}
 			}
 			else
 			{

commit 17c578de5dbc4db0adf7e1123ae4bb9fe240ed83

Attempt to fix regression caused by r16420: when saving a new buffer, it is not the current buffer anymore. See #2887040

org/gjt/sp/jedit/Buffer.java
@@ -2015,7 +2015,6 @@ public class Buffer extends JEditBuffer
 							bufferSets.add(bufferSet);
 					}
 				});
-				jEdit.getBufferSetManager().removeBuffer(this);
 				for (BufferSet bufferSet: bufferSets)
 					jEdit.getBufferSetManager().addBuffer(bufferSet, this);
 			}